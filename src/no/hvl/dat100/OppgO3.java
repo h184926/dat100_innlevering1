863c87300eb3b0e69ab99643d4d3c8dd6894f688
h184926 <annafpkaland@icloud.com>
2024-10-08 14:26:18 +0200

    Update OppgO3.java

--- src/no/hvl/dat100/OppgO3.java
+++ src/no/hvl/dat100/OppgO3.java
@@ -1,5 +1,20 @@
 package no.hvl.dat100;
-
+import java.util.Scanner;
 public class OppgO3 {
+	
+	public static void main(String[] args) {
 
+        Scanner obj = new Scanner(System.in);  // Create a Scanner object
+        System.out.print("Skriv inn n (n>0): ");
+        long n = obj.nextLong();  // Read user input
+
+        long resultat=1;
+        for(long i=2;i<=n;i++){
+                resultat=resultat*i;
+        }System.out.println("n! av " + n + " er " + resultat);
+            
+		
+    obj.close();
+
+	}
 }
